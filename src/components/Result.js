ab8d2d530ade52befcc4692028fb6503fbdde4c5
mingmy00 <149574203+mingMy-00@users.noreply.github.com>
2025-03-07 16:14:03 +0900

    wip(result): 설문 결과 페이지 스타일 임시

--- src/components/Result.js
+++ src/components/Result.js
@@ -2,12 +2,13 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import styles from "../styles/Result.module.css";
+
 export default function ResultPage() {
   const router = useRouter();
   const [product, setProduct] = useState({
     name: "[이니스프리] 블루베리 리밸런싱 5.5 클렌저",
     price: "8,900원",
-    image: "/logo.svg"
+    image: "/groomhim.svg"
   });
 
   const handleGoToHome = () => {
@@ -27,17 +28,17 @@ export default function ResultPage() {
       </div>
 
       <div className={styles.resultBox}>
-        <div className={styles.resultTitle}>
-          <h2>그룸힘의 피부타입은</h2>
-          <h2>1번타입 <span className={styles.highlight}>수부지트러블형</span>입니다.</h2>
+        <div className={styles.resultText}>
+          <p>그룸힘님의 피부타입은</p>
+          <p>1번타입 <span className={styles.highlight}>수부지트러블형</span>입니다.</p>
         </div>
 
         <p className={styles.resultDescription}>
-          수부지 트러블피부는 피지 분비가 많지만 속건조로 인해 수분이 부족한 상태로, 트러블이 쉽게 발생합니다. 이를 관리하기 위해서는 가벼운 수분 크림과 피지 조절 제품을 사용하는 것이 중요합니다. 또한, 과도한 클렌징을 피하고 순한 수분 성분와 진정한 보습을 유지하는 것이 필요합니다.
+          수부지 트러블피부는 피지 분비가 많지만 속건조로 인해 수분이 부족한 상태로, 트러블이 쉽게 발생합니다. 이를 관리하기 위해서는 가벼운 수분크림과 피지 조절 제품을 사용하는 것이 중요합니다. 또한, 과도한 클렌징을 피하고 순한 수분 성분와 적절한 보습을 유지하는 것이 필요합니다.
         </p>
 
         <div className={styles.productSection}>
-          <h3 className={styles.productTitle}>피부타입에 맞는 제품 추천 해드릴게요!</h3>
+          <p className={styles.productTitle}>피부타입에 맞는 제품 추천 해드릴게요!</p>
           
           <div className={styles.productCard}>
             <div className={styles.productImageContainer}>
@@ -48,7 +49,7 @@ export default function ResultPage() {
               />
             </div>
             <div className={styles.productInfo}>
-              <h4 className={styles.productName}>{product.name}</h4>
+              <p className={styles.productName}>{product.name}</p>
               <p className={styles.productPrice}>{product.price}</p>
               <button className={styles.buyButton}>제품 구경하러 가기</button>
             </div>
